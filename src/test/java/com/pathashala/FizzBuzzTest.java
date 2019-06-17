983818567ef210951dc6e2a7d8ae6652c841baac
MonikaMahanthappa <monika@mavenhive.in>
2019-06-17 07:45:00 +0530

    Initialise Project

---
+++ src/test/java/com/pathashala/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package com.pathashala;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzzTest {
+    @Test
+    void dummy() {
+        assertEquals(1, 1);
+    }
+}
