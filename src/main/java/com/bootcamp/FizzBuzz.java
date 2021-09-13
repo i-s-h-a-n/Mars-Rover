96318a13ae31e9319598db5b9c46bc5fe7592e85
monika <monikamahanthappa@gmail.com>
2021-09-13 12:48:23 +0530

    Monika/Jake | Refactor. Extract method to improve readability

--- src/main/java/com/bootcamp/FizzBuzz.java
+++ src/main/java/com/bootcamp/FizzBuzz.java
@@ -2,9 +2,13 @@ package com.bootcamp;
 
 public class FizzBuzz {
     public String run(int number) {
-        if(number % 3 == 0) {
+        if(isMultipleOfThree(number)) {
             return "Fizz";
         }
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOfThree(int number) {
+        return number % 3 == 0;
     }
 }
